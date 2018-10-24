ed96a5a636a3fa8835c008e71a21cd94de8cd913
ZYunH <zyunhjob@163.com>
2018-10-24 17:24:07 +0800

    Update demo/utils.py

--- demo/utils.py
+++ demo/utils.py
@@ -27,69 +27,61 @@ def catch_unknown_error(error, error_info):
     return _wrapper
 
 
-@catch_unknown_error(LoginError, 'Unknown error:')
-async def get_permission(request: aiohttp.web.Request) -> None:
-    """Get permission from UC system."""
-
-    async def get_user_base_info(_session, _cookie):
-        async with _session.get(UC_SYSTEM_URL + '/getLoginStatus', headers={'Cookie': _cookie}) as resp:
-            resp_as_json = await resp.json()
-            success_resp_as_json = schema.Schema({
-                'status': 0,
-                'data':
-                    {
-                        'isLogin': bool,
-                        'userId': int
-                    }
-            }, ignore_extra_keys=True).validate(resp_as_json)
-
-            return success_resp_as_json
-
-    async def get_all_permission_info(_session, _cookie, _user_id: int):
-        async with _session.get(THEMIS_SYSTEM_URL + '/permission/getListByUserId',
-                                headers={'Cookie': _cookie}, params=(('appKey', permissionAppKey),
-                                                                     ('appSecret', permissionAppSecret),
-                                                                     ('userId', str(_user_id)))) as resp:
-            resp_as_json = await resp.json()
-            success_resp_as_json = schema.Schema({
-                'status': 0,
-                'data': list
-            }, ignore_extra_keys=True).validate(resp_as_json)
-
-            return success_resp_as_json
-
-    http_session = request.app['http_session']
-    cookie = request.headers.get('Cookie')
-    if cookie is None:
-        raise LoginError('Can not find header "Cookie".')
-
-    success_uc_resp_as_json = await get_user_base_info(http_session, cookie)
-    user_id = success_uc_resp_as_json['data']['userId']
-    success_themis_resp_as_json = await get_all_permission_info(http_session, cookie, user_id)
-
-    if request.get('user_info') is None:
-        request['user_info'] = {
-            'used_id': success_uc_resp_as_json['data']['userId'],
-            'is_login': success_uc_resp_as_json['data']['isLogin'],
-            'permissions': success_themis_resp_as_json['data']
-        }
-    else:
-        request['user_info']['user_id'] = success_uc_resp_as_json['data']['userId']
-        request['user_info']['is_login'] = success_uc_resp_as_json['data']['isLogin']
-        request['user_info']['permissions'] = success_themis_resp_as_json['data']
-
-
-def assert_permissions(check=True):
+def validate_permission():
     def outer_wrapper(func):
         @wraps(func)
+        @catch_unknown_error(LoginError, 'Login error:')
         async def inner_wrapper(*args, **kwargs):
+
+            async def get_user_id(_session, _cookie) -> int:
+                async with _session.get(UC_SYSTEM_URL + '/getLoginStatus', headers={'Cookie': _cookie}) as resp:
+                    resp_as_json = await resp.json()
+                    success = schema.Schema({
+                        'status': 0,
+                        'data':
+                            {
+                                'isLogin': True,
+                                'userId': int
+                            }
+                    }, ignore_extra_keys=True).is_valid(resp_as_json)
+                    if not success:
+                        raise LoginError('Invalid cookie info.')
+                    return resp_as_json['data']['userId']
+
+            async def _validate_permission(_session, _user_id: int, permission: str) -> bool:
+                async with _session.post(THEMIS_SYSTEM_URL + '/grant/validateByUserId',
+                                         json={'appKey': permissionAppKey,
+                                               'appSecret': permissionAppSecret,
+                                               'userId': _user_id,
+                                               'name': permission}, ssl=False) as resp:
+                    resp_as_json = await resp.json()
+                return schema.Schema({
+                    'status': 0,
+                    'data': {
+                        'hasGranted': True
+                    }
+                }, ignore_extra_keys=True).is_valid(resp_as_json)
+
             request = args[0]  # type: aiohttp.web.Request
-            await get_permission(request)
+            http_session = request.app['http_session']
+            cookie = request.headers.get('Cookie')
+
+            if cookie is None:
+                raise LoginError('Can not find header "Cookie".')
+
+            user_id = await get_user_id(http_session, cookie)
 
             request_uri = ' '.join((request.method, request.path))
 
-            if check and request_uri not in request['user_info']['permissions']:
-                raise LoginError('User can not access this API.')
+            if not await _validate_permission(http_session, user_id, request_uri):
+                raise LoginError('User can not access this API!')
+
+            if request.get('user_info') is None:
+                request['user_info'] = {
+                    'used_id': user_id
+                }
+            else:
+                request['user_info']['user_id'] = user_id
 
             return await func(*args, **kwargs)
 
