2c5137997adba3e39f63aa6d275713808bc62d2e
moonmoonbird <lomoonmoonbird@gmail.com>
2018-12-17 20:12:06 +0800

    增添数据权限

--- user/api.py
+++ user/api.py
@@ -204,7 +204,8 @@ class User(BaseHandler, DataExcludeMixin):
         channel_ids = await channel_oids.to_list(10000)
 
         channel_ids = ','.join([str(id['old_id']) for id in channel_ids])
-        channel_ids = list(set(channel_ids).difference(set(exclude_channels_u)))
+        channel_ids_2 = ','.join([str(id) for id in exclude_channels_u])
+        channel_ids = list(set(channel_ids).difference(set(channel_ids_2)))
         if channel_ids:
             sql = "select id, name from " \
                   "sigma_account_us_user " \
